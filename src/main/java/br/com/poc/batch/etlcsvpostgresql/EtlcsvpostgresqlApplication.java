bdf9a25058e3cc8fab7293efde08f31404005b81
robertoamorimgit <robertasantos@gmail.com>
2026-02-05 19:41:16 -0300

    feat: projeto Spring Boot com Spring Batch configurado

---
+++ src/main/java/br/com/poc/batch/etlcsvpostgresql/EtlcsvpostgresqlApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc.batch.etlcsvpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtlcsvpostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtlcsvpostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/poc/batch/etlcsvpostgresql/EtlcsvpostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.poc.batch.etlcsvpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtlcsvpostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
